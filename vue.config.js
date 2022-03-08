99d82b34275d8a43ac2ca28791178b740ef1b9bb
omrcard88 <omrcard88@gmail.com>
2022-03-08 09:43:31 +0900

    dd

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 module.exports = {
   transpileDependencies: true,
   lintOnSave:true
 }
-=======
-module.exports={
-    lintOnSave: false
-}
->>>>>>> 7545a8bbbedb33de3ba95478820b76e088fb6e27
